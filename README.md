ca647f2817bc858433a6de859b1f7252712c55a8
Prashant <sunspotting@gmail.com>
2021-04-12 14:49:12 -0400

    New spec calls for only daily totals, with a grand total at the end.

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 
-
-:2021-04-06_Tue:
-- 
-
-* 255a6b4 - Tue, 6 Apr 2021 13:14:02 -0400 (5 hours ago) (HEAD -> main, origin/main)
-|           Rubocop suggested:  use snake case. - Prashant
-
-
-* 2292b3b - Tue, 6 Apr 2021 09:30:07 -0400 (8 hours ago)
-            Init. - Prashant
+- Read timestamps of events from a text file:
+  - date format ":2021-04-12_Mon:"
+  - event format "10:13_AM"
 
--- timeseries_processer.rb
+++ timeseries_processer.rb
@@ -1,14 +1,10 @@
 #! /usr/bin/env ruby
 
 module TimeSeriesProcessor
+
   def TimeSeriesProcessor.run(args)
     error_flag = false
-    (file_name, num_lines) = args.map { |i| i.match(/\D/) ? i : i.to_i }
-
-    unless file_name =~ /\W+/ && num_lines.is_a?(Integer)
-      puts "Useage:\n#{__FILE__} filename no_lines"
-      error_flag = true
-    end
+    file_name = args[0]
 
     unless File.exist? file_name
       puts "#{file_name} does not exist."
@@ -18,56 +14,56 @@ module TimeSeriesProcessor
     exit 0 if error_flag
 
     tsp = TimestampProcessor.new(file_name)
-    tsp.run num_lines
+    tsp.run 
   end
 
   class TimestampProcessor
     def initialize(file_name)
       @fp = File.open(file_name)
-      @current_date = ''
-      @num_hits = 0
-      @last_timestamp = ''
+      @current_date = nil
+      @num_hits_day = 0
+      @num_hits_total = 0
+      @last_date = nil
     end
 
-    def run(num_lines)
-      num_lines.times do
-        line = @fp.gets
+    def run
+      while (line = @fp.gets) do
         process_line line
         if @fp.eof # if last line of file, dump last
-          puts "%s, %d" % [@last_timestamp, @num_hits]
+          puts "%s, %d" % [@current_date, @num_hits_day]
+          puts "Total hits: %d" % [@num_hits_total]
           break
         end
       end
     end
 
+    def first_time?
+      !@last_date # true when nil
+    end
+
     def process_line(line)
       case line 
       in /:(\d\d\d\d)-(\d\d)-(\d\d)_.+:/
+        puts "%s, %d" % [@current_date, @num_hits_day] unless first_time?
         (a,b,c) = [$1,$2,$3].map(&:to_i)
-        @current_date = '%4d/%02d/%02d' % [a,b,c]
+        @current_date = '%4d-%02d-%02d' % [a,b,c]
+        @num_hits_day = 0
+        @last_date = @current_date
       in /(\d\d):(\d\d)_(.M)/
-        (hour, min, pm) = [$1.to_i, $2.to_i, $3.downcase]
-        if pm =~ /pm/ && (hour < 12)
-          hour += 12
-        end
-        timestamp = '%s,%02d:%02d' % [@current_date, hour, min]
-        if @last_timestamp.empty? # first time matching
-          @num_hits += 1
-          @last_timestamp = timestamp
-        elsif @last_timestamp == timestamp # repeat
-          @num_hits += 1
-        else # not the same 
-          # print last match in cycle
-          puts "%s, %d" % [@last_timestamp, @num_hits]
-          # start new cycle
-          @last_timestamp = timestamp
-          @num_hits = 1
-        end
+        @num_hits_day += 1
+        @num_hits_total += 1
       else
-        # Lines that don't match are ignored
+        # ignore all else
       end
     end
   end
 end
 
 TimeSeriesProcessor.run(ARGV)
+
+__END__
+Go.state(:also) do |also|
+
+  also.
+
+end
